9a274747d6b165d925dd68c198029cb97db5f5fc
Admin <admin@chrisoldwood.com>
2008-09-27 20:57:04 +0000

    Initial version.

---
+++ Templates/Struct.hpp
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   $File$.hpp
+//! \brief  The $CLASS$ structure declaration.
+//! \author Chris Oldwood
+
+// Check for previous inclusion
+#ifndef $LIB_$$FILE$_HPP
+#define $LIB_$$FILE$_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+namespace $NAMESPACE$
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//!
+
+struct $CLASS$
+{
+	//
+	// Members.
+	//
+
+	//! Default constructor.
+	$CLASS$();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//! Default constructor.
+
+inline $CLASS$::$CLASS$()
+{
+}
+
+//namespace $NAMESPACE$
+}
+
+#endif // $LIB_$$FILE$_HPP
