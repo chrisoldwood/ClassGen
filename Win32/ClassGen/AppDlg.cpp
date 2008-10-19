ec3ed5d5b2a93f6758764863f2a75ab42d6fabf8
Admin <admin@chrisoldwood.com>
2008-10-19 20:11:40 +0000

    Moved GenerateFile() method to AppCmds ready for command line use. Added support for editing the Namespace name. Rationalised the parameter names so that they all have an upper, mixed and lower case version. Fixed the bug where the .hpp or .cpp filename wasn't regenerated on a template change. Changed filenames to be empty if the class is empty instead of just having the .hpp and .cpp extensions. Added support for showing the Component and Template descriptions on selection.

--- Win32/ClassGen/AppDlg.cpp
+++ Win32/ClassGen/AppDlg.cpp
@@ -1,35 +1,18 @@
-/******************************************************************************
-** (C) Chris Oldwood
-**
-** MODULE:		APPDLG.CPP
-** COMPONENT:	The Application.
-** DESCRIPTION:	CAppDlg class definition.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! \file   AppDlg.cpp
+//! \brief  The AppDlg class definition.
+//! \author Chris Oldwood
 
 #include "Common.hpp"
 #include "AppDlg.hpp"
 #include "AboutDlg.hpp"
-#include <WCL/File.hpp>
-#include <WCL/FileException.hpp>
 #include "ClassGenApp.hpp"
 #include "Params.hpp"
-#include <Core/AnsiWide.hpp>
 
-/******************************************************************************
-** Method:		Default constructor.
-**
-** Description:	.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Default constructor.
 
-CAppDlg::CAppDlg()
+AppDlg::AppDlg()
 	: CMainDlg(IDD_MAIN)
 {
 
@@ -40,157 +23,145 @@ CAppDlg::CAppDlg()
 		CTRL(IDC_FOLDER,		&m_cbFolder)
 		CTRL(IDC_HPP_FILE,		&m_ebHPPFile)
 		CTRL(IDC_CPP_FILE,		&m_ebCPPFile)
+		CTRL(IDC_NAMESPACE,		&m_namespaceEditor)
+		CTRL(IDC_COMP_DESC,		&m_componentLabel)
+		CTRL(IDC_TMPL_DESC,		&m_templateLabel)
 	END_CTRL_TABLE
 
 	DEFINE_CTRLMSG_TABLE
-		CMD_CTRLMSG(IDC_COMPONENT,	CBN_SELCHANGE,	&CAppDlg::OnSelectComponent)
-		CMD_CTRLMSG(IDC_TEMPLATE,	CBN_SELCHANGE,	&CAppDlg::OnSelectTemplate)
-		CMD_CTRLMSG(IDC_CLOSE,		BN_CLICKED,		&CAppDlg::OnClose)
-		CMD_CTRLMSG(IDC_BROWSE,		BN_CLICKED,		&CAppDlg::OnBrowse)
-		CMD_CTRLMSG(IDC_CLASS_NAME,	EN_CHANGE,		&CAppDlg::OnEditClassName)
-		CMD_CTRLMSG(IDC_GENERATE,	BN_CLICKED,		&CAppDlg::OnGenerate)
-		CMD_CTRLMSG(IDC_ABOUT,		BN_CLICKED,		&CAppDlg::OnAbout)
+		CMD_CTRLMSG(IDC_COMPONENT,	CBN_SELCHANGE,	&AppDlg::OnSelectComponent)
+		CMD_CTRLMSG(IDC_TEMPLATE,	CBN_SELCHANGE,	&AppDlg::OnSelectTemplate)
+		CMD_CTRLMSG(IDC_CLOSE,		BN_CLICKED,		&AppDlg::OnClose)
+		CMD_CTRLMSG(IDC_BROWSE,		BN_CLICKED,		&AppDlg::OnBrowse)
+		CMD_CTRLMSG(IDC_CLASS_NAME,	EN_CHANGE,		&AppDlg::OnEditClassName)
+		CMD_CTRLMSG(IDC_GENERATE,	BN_CLICKED,		&AppDlg::OnGenerate)
+		CMD_CTRLMSG(IDC_ABOUT,		BN_CLICKED,		&AppDlg::OnAbout)
 	END_CTRLMSG_TABLE
 }
 
-/******************************************************************************
-** Method:		OnInitDialog()
-**
-** Description:	Initialise the dialog.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Handle dialog creation.
 
-void CAppDlg::OnInitDialog()
+void AppDlg::OnInitDialog()
 {
-	if (App.m_aoTemplates.empty())
+	if (g_app.m_templates.empty())
 	{
 		AlertMsg(TXT("There are no templates defined."));
-		App.m_AppWnd.Close();
+		g_app.m_appWnd.Close();
 		return;
 	}
 
-	if (App.m_aoComponents.empty())
+	if (g_app.m_components.empty())
 	{
 		AlertMsg(TXT("There are no components defined."));
-		App.m_AppWnd.Close();
+		g_app.m_appWnd.Close();
 		return;
 	}
 
 	// Load template names combo.
-	for (uint i = 0; i < App.m_aoTemplates.size(); ++i)
-		m_cbTemplate.Add(App.m_aoTemplates[i]->m_strName, i);
+	for (size_t i = 0; i < g_app.m_templates.size(); ++i)
+	{
+		TemplatePtr templ = g_app.m_templates[i];
+		tstring     item  = templ->m_name;
+
+		m_cbTemplate.Add(item.c_str(), i);
+	}
 
 	// Select 1st template by default.
-	m_cbTemplate.CurSel(m_cbTemplate.FindExact(App.m_aoTemplates[0]->m_strName));
+	m_cbTemplate.CurSel(m_cbTemplate.Find(g_app.m_templates[0]->m_name.c_str()));
 
 	OnSelectTemplate();
 
 	// Load component names combo.
-	for (uint i = 0; i < App.m_aoComponents.size(); ++i)
-		m_cbComponent.Add(App.m_aoComponents[i]->m_strName, i);
+	for (size_t i = 0; i < g_app.m_components.size(); ++i)
+	{
+		ComponentPtr component = g_app.m_components[i];
+		tstring      item      = component->m_name;
+
+		m_cbComponent.Add(item.c_str(), i);
+	}
 
 	// Make the last used component the default selection.
-	size_t nComponent = m_cbComponent.FindExact(App.m_strLastComponent);
+	size_t nComponent = m_cbComponent.Find(g_app.m_strLastComponent);
 
 	// If none, pick the first component available.
-	if ( (nComponent == Core::npos) && (!App.m_aoComponents.empty()) )
-		nComponent = m_cbComponent.FindExact(App.m_aoComponents[0]->m_strName);
+	if ( (nComponent == Core::npos) && (!g_app.m_components.empty()) )
+		nComponent = m_cbComponent.Find(g_app.m_components[0]->m_name.c_str());
 
 	// Set selection, if possible.
 	if (nComponent != Core::npos)
 		m_cbComponent.CurSel(nComponent);
 
+	OnSelectComponent();
+
 	// Load last used folders combo.
-	for (size_t i = 0; i < App.m_astrFolders.Size(); ++i)
-		AddFolderName(App.m_astrFolders[i], false);
+	for (size_t i = 0; i < g_app.m_astrFolders.Size(); ++i)
+		AddFolderName(tstring(g_app.m_astrFolders[i]), false);
 
 	// Initialise with last settings.
-	AddFolderName(App.m_strLastFolder, true);
+	AddFolderName(tstring(g_app.m_strLastFolder), true);
 }
 
-/******************************************************************************
-** Method:		OnDestroy()
-**
-** Description:	Window being destroyed, save any preferences.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Handle dialog destruction.
 
-void CAppDlg::OnDestroy()
+void AppDlg::OnDestroy()
 {
 	// Use path as starting point for next selection.
-	App.m_strLastComponent = m_cbComponent.Text();
-	App.m_strLastFolder = m_cbFolder.Text();
+	g_app.m_strLastComponent = m_cbComponent.Text();
+	g_app.m_strLastFolder = m_cbFolder.Text();
 }
 
-/******************************************************************************
-** Method:		OnClose()
-**
-** Description:	Close the application.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-void CAppDlg::OnClose()
+void AppDlg::OnClose()
 {
-	App.m_AppWnd.Close();
+	g_app.m_appWnd.Close();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Handle the selection of a new component.
 
-void CAppDlg::OnSelectComponent()
+void AppDlg::OnSelectComponent()
 {
+	ASSERT(m_cbComponent.Count() > 0);
+
 	// Get the selected component.
 	int nSel  = m_cbComponent.CurSel();
 	int nItem = m_cbComponent.ItemData(nSel);
 
-	CComponentPtr pComponent = App.m_aoComponents[nItem];
+	ComponentPtr pComponent = g_app.m_components[nItem];
 
 	// Update controls.
-	if (!pComponent->m_strFolder.Empty())
-		AddFolderName(pComponent->m_strFolder, true);
+	if (!pComponent->m_folder.empty())
+		AddFolderName(pComponent->m_folder, true);
+
+	m_namespaceEditor.Text(pComponent->m_namespace.c_str());
+	m_componentLabel.Text(pComponent->m_description.c_str());
 }
 
-/******************************************************************************
-** Method:		OnSelectTemplate()
-**
-** Description:	Template selection changed.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-void CAppDlg::OnSelectTemplate()
+void AppDlg::OnSelectTemplate()
 {
+	ASSERT(m_cbTemplate.Count() > 0);
+
 	// Get the selected template.
 	int nSel  = m_cbTemplate.CurSel();
 	int nItem = m_cbTemplate.ItemData(nSel);
 
-	CTemplatePtr pTemplate = App.m_aoTemplates[nItem];
+	TemplatePtr pTemplate = g_app.m_templates[nItem];
 
 	// Get fields expected.
-	bool bIsClass = pTemplate->m_bNeedsClass;
-	bool bHasHPP  = (pTemplate->m_strHPPFile != TXT(""));
-	bool bHasCPP  = (pTemplate->m_strCPPFile != TXT(""));
+	bool bIsClass = pTemplate->m_isClass;
+	bool bHasHPP  = !pTemplate->m_headerFile.empty();
+	bool bHasCPP  = !pTemplate->m_sourceFile.empty();
 
 	// Update controls.
+	m_templateLabel.Text(pTemplate->m_description.c_str());
+
 	m_ebClassName.Enable(bIsClass);
 
 	if (!bIsClass)
@@ -205,121 +176,96 @@ void CAppDlg::OnSelectTemplate()
 
 	if (!bHasCPP)
 		m_ebCPPFile.Text(TXT(""));
+
+	// Regenerate file names, if necessary.
+	if (m_ebClassName.TextLength() > 0)
+		OnEditClassName();
 }
 
-/******************************************************************************
-** Method:		OnEditClassName()
-**
-** Description:	Class name changed, update the hpp and cpp file names.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-void CAppDlg::OnEditClassName()
+void AppDlg::OnEditClassName()
 {
-	CString strClassName = m_ebClassName.Text();
+	// Clear the filenames.
+	if (m_ebClassName.TextLength() == 0)
+	{
+		m_ebHPPFile.Text(TXT(""));
+		m_ebCPPFile.Text(TXT(""));
+	}
+	// Generate new filenames.
+	else
+	{
+		CString strClassName = m_ebClassName.Text();
 
-	// Set the filenames.
-	if (m_ebHPPFile.IsEnabled())
-		m_ebHPPFile.Text(strClassName + App.m_strHppExt);
+		if (m_ebHPPFile.IsEnabled())
+			m_ebHPPFile.Text(strClassName + g_app.m_strHppExt);
 
-	if (m_ebCPPFile.IsEnabled())
-		m_ebCPPFile.Text(strClassName + App.m_strCppExt);
+		if (m_ebCPPFile.IsEnabled())
+			m_ebCPPFile.Text(strClassName + g_app.m_strCppExt);
+	}
 }
 
-/******************************************************************************
-** Method:		OnBrowse()
-**
-** Description:	Browse for the hpp/cpp file destination folder.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-void CAppDlg::OnBrowse()
+void AppDlg::OnBrowse()
 {
 	CPath strFolder = m_cbFolder.Text();
 
 	// If folder name empty start from last path.
 	if (strFolder == TXT(""))
-		strFolder = App.m_strLastFolder;
+		strFolder = g_app.m_strLastFolder;
 
 	// Select the folder.
 	if (!strFolder.SelectDir(*this, TXT("Select The Destination Folder"), strFolder))
 		return;
 
 	// Display path.
-	AddFolderName(strFolder, true);
+	AddFolderName(tstring(strFolder), true);
 }
 
-/******************************************************************************
-** Method:		AddFolderName()
-**
-** Description:	Add the folder name to the combo if it doesn't already exist.
-**				Make if the selection, if required.
-**
-** Parameters:	strName		The folder name.
-**				bSelect		Make it the selection.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-void CAppDlg::AddFolderName(const CString& strName, bool bSelect)
+void AppDlg::AddFolderName(const tstring& name, bool select)
 {
 	// Discard if empty.
-	if (strName.Empty())
+	if (name.empty())
 		return;
 
 	// Already exists?
-	int nPos = m_cbFolder.FindExact(strName);
+	size_t pos = m_cbFolder.FindExact(name.c_str());
 
-	if (nPos == CB_ERR)
-		nPos = m_cbFolder.Add(strName);
+	if (pos == Core::npos)
+		pos = m_cbFolder.Add(name.c_str());
 
 	// Make it selection?
-	if (bSelect)
-		m_cbFolder.CurSel(nPos);
+	if (select)
+		m_cbFolder.CurSel(pos);
 }
 
-/******************************************************************************
-** Method:		OnGenerate()
-**
-** Description:	Generate the class.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-void CAppDlg::OnGenerate()
+void AppDlg::OnGenerate()
 {
 	// Get the selected template.
 	int nTemplSel  = m_cbTemplate.CurSel();
 	int nTemplItem = m_cbTemplate.ItemData(nTemplSel);
 
-	CTemplatePtr pTemplate = App.m_aoTemplates[nTemplItem];
+	TemplatePtr pTemplate = g_app.m_templates[nTemplItem];
 
 	// Get the selected template.
 	int nComptSel  = m_cbComponent.CurSel();
 	int nComptItem = m_cbComponent.ItemData(nComptSel);
 
-	CComponentPtr pComponent = App.m_aoComponents[nComptItem];
+	ComponentPtr pComponent = g_app.m_components[nComptItem];
 
 	// Get expected fields.
-	bool bIsClass = pTemplate->m_bNeedsClass;
-	bool bHasHPP  = (pTemplate->m_strHPPFile != TXT(""));
-	bool bHasCPP  = (pTemplate->m_strCPPFile != TXT(""));
+	bool bIsClass = pTemplate->m_isClass;
+	bool bHasHPP  = !pTemplate->m_headerFile.empty();
+	bool bHasCPP  = !pTemplate->m_sourceFile.empty();
 
 	// Validate class name.
 	CString strClassName = m_ebClassName.Text();
@@ -361,43 +307,55 @@ void CAppDlg::OnGenerate()
 		return;
 	}
 
-	CParams oParams;
+	Params params;
 
-	oParams.Set(TXT("CLASS"),     strClassName);
-	oParams.Set(TXT("INCLUDE"),   pComponent->m_strInclude);
-	oParams.Set(TXT("COMPONENT"), pComponent->m_strComment);
-	oParams.Set(TXT("NAMESPACE"), pComponent->m_strNamespace);
-
-	if (!pComponent->m_strNamespace.Empty())
-		oParams.Set(TXT("LIB_"), CString(pComponent->m_strNamespace + TXT("_")).ToUpper());
+	params.add(TXT("Class"), tstring(strClassName));
+	params.add(TXT("Component"), pComponent->m_name);
+	params.add(TXT("Include"), pComponent->m_include);
+	params.add(TXT("Comment"), pComponent->m_comment);
+	params.add(TXT("Namespace"), tstring(m_namespaceEditor.Text()));
+	params.add(TXT("Author"), g_app.m_author);
 
 	// Generate HPP file, if required.
-	if (pTemplate->m_strHPPFile != TXT(""))
+	if (!pTemplate->m_headerFile.empty())
 	{
-		CPath strTemplateFile = CPath(App.m_strTmplFolder, pTemplate->m_strHPPFile);
+		CPath   strTemplateFile = CPath(g_app.m_strTmplFolder, pTemplate->m_headerFile.c_str());
+		CString strFileTitle    = strHPPFile.FileTitle();
+		CString strFileExt      = strHPPFile.FileExt();
 
-		oParams.Set(TXT("File"), strHPPFile.FileTitle());
-		oParams.Set(TXT("FILE"), strHPPFile.FileTitle().ToUpper());
+		if (!strFileExt.Empty())
+			strFileExt.Delete(0);
 
-		if (!GenerateFile(strTemplateFile, CPath(strFolder, strHPPFile), oParams))
+		params.add(TXT("File.Ext"), tstring(strHPPFile));
+		params.add(TXT("File"), tstring(strFileTitle));
+		params.add(TXT("Ext"), tstring(strFileExt));
+		params.add(TXT("HdrExt"), tstring(strFileExt));
+
+		if (!g_app.m_appCmds.generateFile(strTemplateFile, CPath(strFolder, strHPPFile), params))
 			return;
 	}
 
 	// Generate CPP file, if required.
-	if (pTemplate->m_strCPPFile != TXT(""))
+	if (!pTemplate->m_sourceFile.empty())
 	{
-		CPath strTemplateFile = CPath(App.m_strTmplFolder, pTemplate->m_strCPPFile);
+		CPath   strTemplateFile = CPath(g_app.m_strTmplFolder, pTemplate->m_sourceFile.c_str());
+		CString strFileTitle    = strCPPFile.FileTitle();
+		CString strFileExt      = strCPPFile.FileExt();
 
-		oParams.Set(TXT("File"), strHPPFile.FileTitle());
-		oParams.Set(TXT("FILE"), strCPPFile.FileTitle().ToUpper());
+		if (!strFileExt.Empty())
+			strFileExt.Delete(0);
 
-		if (!GenerateFile(strTemplateFile, CPath(strFolder, strCPPFile), oParams))
+		params.add(TXT("File.Ext"), tstring(strCPPFile));
+		params.add(TXT("File"), tstring(strFileTitle));
+		params.add(TXT("Ext"), tstring(strFileExt));
+
+		if (!g_app.m_appCmds.generateFile(strTemplateFile, CPath(strFolder, strCPPFile), params))
 			return;
 	}
 
 	// Add folder to MRU list.
-	if (App.m_astrFolders.Find(strFolder, true) == -1)
-		App.m_astrFolders.Add(strFolder);
+	if (g_app.m_astrFolders.Find(strFolder, true) == -1)
+		g_app.m_astrFolders.Add(strFolder);
 
 	if (!strClassName.Empty())
 		NotifyMsg(TXT("%s class generated."), strClassName);
@@ -405,139 +363,12 @@ void CAppDlg::OnGenerate()
 		NotifyMsg(TXT("%s file generated."), strHPPFile);
 }
 
-/******************************************************************************
-** Method:		GenerateFile()
-**
-** Description:	Generate a target file from a template.
-**
-** Parameters:	strTemplateFile		The template filename.
-**				strTargetFile		The target filename.
-**				oParams				The substitution parameters.
-**
-** Returns:		true or false.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//!
 
-bool CAppDlg::GenerateFile(const CPath& strTemplateFile, const CPath& strTargetFile, const CParams& oParams)
+void AppDlg::OnAbout()
 {
-	// Check template file exists.
-	if (!strTemplateFile.Exists())
-	{
-		AlertMsg(TXT("The template file is missing:\n\n%s"), strTemplateFile);
-		return false;
-	}
+	AboutDlg aboutDlg;
 
-	// Check template file is writable.
-	if (strTemplateFile.ReadOnly())
-	{
-		AlertMsg(TXT("The template file is write-protected:\n\n%s"), strTemplateFile);
-		return false;
-	}
-
-	// Check target file doesn't exist.
-	if (strTargetFile.Exists())
-	{
-		if (QueryMsg(TXT("The target file already exists:\n\n%s\n\nDo you want to overwrite it?"), strTargetFile) != IDYES)
-			return false;
-	}
-
-	// Copy template to target file.
-	if (!CFile::Copy(strTemplateFile, strTargetFile, true))
-	{
-		AlertMsg(TXT("Failed to create target file:\n\n%s"), strTargetFile);
-		return false;
-	}
-
-	try
-	{
-		CFile oFile;
-
-		oFile.Open(strTargetFile, GENERIC_READWRITE);
-
-		// Get the files size.
-		ulong lSize = oFile.Size();
-
-		// Allocate zero terminated code buffer.
-		char* pszCode = static_cast<char*>(_alloca(lSize+1));
-
-		// Read file contents into tmp buffer.
-		oFile.Read(pszCode, lSize);
-		pszCode[lSize] = '\0';
-
-		// Reset for writing.
-		oFile.Seek(0, FILE_BEGIN);
-
-		char* pszEnd = pszCode + lSize;
-
-		// For all characters...
-		while (*pszCode != '\0')
-		{
-			// Finds the next parameter to substitute.
-			char* pszParam = strchr(pszCode, '$');
-
-			// No more parameters to handle?
-			if (pszParam == NULL)
-			{
-				// Write the rest of the file.
-				oFile.Write(pszCode, pszEnd - pszCode);
-
-				pszCode = pszEnd;
-			}
-			else // (pszParam != NULL)
-			{
-				// Write the code up to the parameter.
-				oFile.Write(pszCode, pszParam - pszCode);
-
-				// Find the end of the parameter name.
-				char* pszParamEnd = strchr(pszParam+1, '$');
-
-				// Parameter to substitue?
-				if (pszParamEnd != NULL)
-				{
-					std::string strParam(pszParam+1, pszParamEnd-pszParam-1);
-					std::string strValue(T2A(oParams.Find(A2T(strParam))));
-
-					// Write the substituted value.
-					oFile.Write(strValue.c_str(), strValue.length());
-
-					pszCode = pszParamEnd+1;
-				}
-				else // (pszParamEnd == NULL)
-				{
-					// Invalid parameter, skip passed token.
-					pszCode++;
-				}
-			}
-		}
-
-		oFile.SetEOF();
-		oFile.Close();
-	}
-	catch (CFileException& e)
-	{
-		AlertMsg(TXT("Failed to parse target file:\n\n%s\n\nReason: %s"), strTargetFile, e.What());
-		return false;
-	}
-
-	return true;
-}
-
-/******************************************************************************
-** Method:		OnAbout()
-**
-** Description:	Show the About box.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
-
-void CAppDlg::OnAbout()
-{
-	CAboutDlg Dlg;
-
-	Dlg.RunModal(*this);
+	aboutDlg.RunModal(*this);
 }
