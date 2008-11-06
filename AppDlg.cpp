f8cd9805acdf1d7b27243c7e65097bcf4727b264
Admin <admin@chrisoldwood.com>
2008-11-06 23:15:28 +0000

    Added support for the Help button. Changed the HdrExt parameter to Header. Removed use of Hungarian Notation.

--- AppDlg.cpp
+++ AppDlg.cpp
@@ -8,6 +8,7 @@
 #include "AboutDlg.hpp"
 #include "ClassGenApp.hpp"
 #include "Params.hpp"
+#include <WCL/BusyCursor.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Default constructor.
@@ -17,25 +18,26 @@ AppDlg::AppDlg()
 {
 
 	DEFINE_CTRL_TABLE
-		CTRL(IDC_TEMPLATE,		&m_cbTemplate)
-		CTRL(IDC_COMPONENT,		&m_cbComponent)
-		CTRL(IDC_CLASS_NAME,	&m_ebClassName)
-		CTRL(IDC_FOLDER,		&m_cbFolder)
-		CTRL(IDC_HPP_FILE,		&m_ebHPPFile)
-		CTRL(IDC_CPP_FILE,		&m_ebCPPFile)
+		CTRL(IDC_TEMPLATE,		&m_templateList)
+		CTRL(IDC_COMPONENT,		&m_componentList)
+		CTRL(IDC_CLASS_NAME,	&m_classNameEditor)
+		CTRL(IDC_FOLDER,		&m_folderList)
+		CTRL(IDC_HPP_FILE,		&m_hppFileEditor)
+		CTRL(IDC_CPP_FILE,		&m_cppFileEditor)
 		CTRL(IDC_NAMESPACE,		&m_namespaceEditor)
 		CTRL(IDC_COMP_DESC,		&m_componentLabel)
 		CTRL(IDC_TMPL_DESC,		&m_templateLabel)
 	END_CTRL_TABLE
 
 	DEFINE_CTRLMSG_TABLE
-		CMD_CTRLMSG(IDC_COMPONENT,	CBN_SELCHANGE,	&AppDlg::OnSelectComponent)
-		CMD_CTRLMSG(IDC_TEMPLATE,	CBN_SELCHANGE,	&AppDlg::OnSelectTemplate)
-		CMD_CTRLMSG(IDC_CLOSE,		BN_CLICKED,		&AppDlg::OnClose)
-		CMD_CTRLMSG(IDC_BROWSE,		BN_CLICKED,		&AppDlg::OnBrowse)
-		CMD_CTRLMSG(IDC_CLASS_NAME,	EN_CHANGE,		&AppDlg::OnEditClassName)
-		CMD_CTRLMSG(IDC_GENERATE,	BN_CLICKED,		&AppDlg::OnGenerate)
-		CMD_CTRLMSG(IDC_ABOUT,		BN_CLICKED,		&AppDlg::OnAbout)
+		CMD_CTRLMSG(IDC_COMPONENT,	CBN_SELCHANGE,	&AppDlg::onSelectComponent)
+		CMD_CTRLMSG(IDC_TEMPLATE,	CBN_SELCHANGE,	&AppDlg::onSelectTemplate)
+		CMD_CTRLMSG(IDC_CLOSE,		BN_CLICKED,		&AppDlg::onClose)
+		CMD_CTRLMSG(IDC_BROWSE,		BN_CLICKED,		&AppDlg::onBrowse)
+		CMD_CTRLMSG(IDC_CLASS_NAME,	EN_CHANGE,		&AppDlg::onEditClassName)
+		CMD_CTRLMSG(IDC_GENERATE,	BN_CLICKED,		&AppDlg::onGenerate)
+		CMD_CTRLMSG(IDC_MANUAL,		BN_CLICKED,		&AppDlg::onHelp)
+		CMD_CTRLMSG(IDC_ABOUT,		BN_CLICKED,		&AppDlg::onAbout)
 	END_CTRLMSG_TABLE
 }
 
@@ -64,13 +66,13 @@ void AppDlg::OnInitDialog()
 		TemplatePtr templ = g_app.m_templates[i];
 		tstring     item  = templ->m_name;
 
-		m_cbTemplate.Add(item.c_str(), i);
+		m_templateList.Add(item.c_str(), i);
 	}
 
 	// Select 1st template by default.
-	m_cbTemplate.CurSel(m_cbTemplate.Find(g_app.m_templates[0]->m_name.c_str()));
+	m_templateList.CurSel(m_templateList.Find(g_app.m_templates[0]->m_name.c_str()));
 
-	OnSelectTemplate();
+	onSelectTemplate();
 
 	// Load component names combo.
 	for (size_t i = 0; i < g_app.m_components.size(); ++i)
@@ -78,28 +80,28 @@ void AppDlg::OnInitDialog()
 		ComponentPtr component = g_app.m_components[i];
 		tstring      item      = component->m_name;
 
-		m_cbComponent.Add(item.c_str(), i);
+		m_componentList.Add(item.c_str(), i);
 	}
 
 	// Make the last used component the default selection.
-	size_t nComponent = m_cbComponent.Find(g_app.m_strLastComponent);
+	size_t component = m_componentList.Find(g_app.m_lastComponent);
 
 	// If none, pick the first component available.
-	if ( (nComponent == Core::npos) && (!g_app.m_components.empty()) )
-		nComponent = m_cbComponent.Find(g_app.m_components[0]->m_name.c_str());
+	if ( (component == Core::npos) && (!g_app.m_components.empty()) )
+		component = m_componentList.Find(g_app.m_components[0]->m_name.c_str());
 
 	// Set selection, if possible.
-	if (nComponent != Core::npos)
-		m_cbComponent.CurSel(nComponent);
+	if (component != Core::npos)
+		m_componentList.CurSel(component);
 
-	OnSelectComponent();
+	onSelectComponent();
 
 	// Load last used folders combo.
-	for (size_t i = 0; i < g_app.m_astrFolders.Size(); ++i)
-		AddFolderName(tstring(g_app.m_astrFolders[i]), false);
+	for (size_t i = 0; i < g_app.m_mruFolders.Size(); ++i)
+		addFolderName(tstring(g_app.m_mruFolders[i]), false);
 
 	// Initialise with last settings.
-	AddFolderName(tstring(g_app.m_strLastFolder), true);
+	addFolderName(tstring(g_app.m_lastFolder), true);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -108,14 +110,14 @@ void AppDlg::OnInitDialog()
 void AppDlg::OnDestroy()
 {
 	// Use path as starting point for next selection.
-	g_app.m_strLastComponent = m_cbComponent.Text();
-	g_app.m_strLastFolder = m_cbFolder.Text();
+	g_app.m_lastComponent = m_componentList.Text();
+	g_app.m_lastFolder = m_folderList.Text();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Handle the close button being pressed.
 
-void AppDlg::OnClose()
+void AppDlg::onClose()
 {
 	g_app.m_appWnd.Close();
 }
@@ -123,250 +125,267 @@ void AppDlg::OnClose()
 ////////////////////////////////////////////////////////////////////////////////
 //! Handle the selection of a new component.
 
-void AppDlg::OnSelectComponent()
+void AppDlg::onSelectComponent()
 {
-	ASSERT(m_cbComponent.Count() > 0);
+	ASSERT(m_componentList.Count() > 0);
 
 	// Get the selected component.
-	int nSel  = m_cbComponent.CurSel();
-	int nItem = m_cbComponent.ItemData(nSel);
+	size_t sel  = m_componentList.CurSel();
+	size_t item = m_componentList.ItemData(sel);
 
-	ComponentPtr pComponent = g_app.m_components[nItem];
+	ComponentPtr pComponent = g_app.m_components[item];
 
 	// Update controls.
 	if (!pComponent->m_folder.empty())
-		AddFolderName(pComponent->m_folder, true);
+		addFolderName(pComponent->m_folder, true);
 
 	m_namespaceEditor.Text(pComponent->m_namespace.c_str());
 	m_componentLabel.Text(pComponent->m_description.c_str());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Handle the selection of a new template.
 
-void AppDlg::OnSelectTemplate()
+void AppDlg::onSelectTemplate()
 {
-	ASSERT(m_cbTemplate.Count() > 0);
+	ASSERT(m_templateList.Count() > 0);
 
 	// Get the selected template.
-	int nSel  = m_cbTemplate.CurSel();
-	int nItem = m_cbTemplate.ItemData(nSel);
+	size_t sel  = m_templateList.CurSel();
+	size_t item = m_templateList.ItemData(sel);
 
-	TemplatePtr pTemplate = g_app.m_templates[nItem];
+	TemplatePtr pTemplate = g_app.m_templates[item];
 
 	// Get fields expected.
-	bool bIsClass = pTemplate->m_isClass;
-	bool bHasHPP  = !pTemplate->m_headerFile.empty();
-	bool bHasCPP  = !pTemplate->m_sourceFile.empty();
+	bool isClass = pTemplate->m_isClass;
+	bool hasHPP  = !pTemplate->m_headerFile.empty();
+	bool hasCPP  = !pTemplate->m_sourceFile.empty();
 
 	// Update controls.
 	m_templateLabel.Text(pTemplate->m_description.c_str());
 
-	m_ebClassName.Enable(bIsClass);
+	m_classNameEditor.Enable(isClass);
 
-	if (!bIsClass)
-		m_ebClassName.Text(TXT(""));
+	if (!isClass)
+		m_classNameEditor.Text(TXT(""));
 
-	m_ebHPPFile.Enable(bHasHPP);
+	m_hppFileEditor.Enable(hasHPP);
 
-	if (!bHasHPP)
-		m_ebHPPFile.Text(TXT(""));
+	if (!hasHPP)
+		m_hppFileEditor.Text(TXT(""));
 
-	m_ebCPPFile.Enable(bHasCPP);
+	m_cppFileEditor.Enable(hasCPP);
 
-	if (!bHasCPP)
-		m_ebCPPFile.Text(TXT(""));
+	if (!hasCPP)
+		m_cppFileEditor.Text(TXT(""));
 
 	// Regenerate file names, if necessary.
-	if (m_ebClassName.TextLength() > 0)
-		OnEditClassName();
+	if (m_classNameEditor.TextLength() > 0)
+		onEditClassName();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Handle a change to the class name.
 
-void AppDlg::OnEditClassName()
+void AppDlg::onEditClassName()
 {
 	// Clear the filenames.
-	if (m_ebClassName.TextLength() == 0)
+	if (m_classNameEditor.TextLength() == 0)
 	{
-		m_ebHPPFile.Text(TXT(""));
-		m_ebCPPFile.Text(TXT(""));
+		m_hppFileEditor.Text(TXT(""));
+		m_cppFileEditor.Text(TXT(""));
 	}
 	// Generate new filenames.
 	else
 	{
-		CString strClassName = m_ebClassName.Text();
+		CString className = m_classNameEditor.Text();
 
-		if (m_ebHPPFile.IsEnabled())
-			m_ebHPPFile.Text(strClassName + g_app.m_strHppExt);
+		if (m_hppFileEditor.IsEnabled())
+			m_hppFileEditor.Text(className + g_app.m_headerExt);
 
-		if (m_ebCPPFile.IsEnabled())
-			m_ebCPPFile.Text(strClassName + g_app.m_strCppExt);
+		if (m_cppFileEditor.IsEnabled())
+			m_cppFileEditor.Text(className + g_app.m_sourceExt);
 	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Show the common dialog to select a folder.
 
-void AppDlg::OnBrowse()
+void AppDlg::onBrowse()
 {
-	CPath strFolder = m_cbFolder.Text();
+	CPath folder = m_folderList.Text();
 
 	// If folder name empty start from last path.
-	if (strFolder == TXT(""))
-		strFolder = g_app.m_strLastFolder;
+	if (folder == TXT(""))
+		folder = g_app.m_lastFolder;
 
 	// Select the folder.
-	if (!strFolder.SelectDir(*this, TXT("Select The Destination Folder"), strFolder))
+	if (!folder.SelectDir(*this, TXT("Select The Destination Folder"), folder))
 		return;
 
 	// Display path.
-	AddFolderName(tstring(strFolder), true);
+	addFolderName(tstring(folder), true);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Add a folder to the folder MRU list. This ensures that duplicates are not
+//! added and can set it as the current selection.
 
-void AppDlg::AddFolderName(const tstring& name, bool select)
+void AppDlg::addFolderName(const tstring& name, bool select)
 {
 	// Discard if empty.
 	if (name.empty())
 		return;
 
 	// Already exists?
-	size_t pos = m_cbFolder.FindExact(name.c_str());
+	size_t pos = m_folderList.FindExact(name.c_str());
 
 	if (pos == Core::npos)
-		pos = m_cbFolder.Add(name.c_str());
+		pos = m_folderList.Add(name.c_str());
 
 	// Make it selection?
 	if (select)
-		m_cbFolder.CurSel(pos);
+		m_folderList.CurSel(pos);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Generate the new class.
 
-void AppDlg::OnGenerate()
+void AppDlg::onGenerate()
 {
 	// Get the selected template.
-	int nTemplSel  = m_cbTemplate.CurSel();
-	int nTemplItem = m_cbTemplate.ItemData(nTemplSel);
+	int templSel  = m_templateList.CurSel();
+	int templItem = m_templateList.ItemData(templSel);
 
-	TemplatePtr pTemplate = g_app.m_templates[nTemplItem];
+	TemplatePtr templt = g_app.m_templates[templItem];
 
 	// Get the selected template.
-	int nComptSel  = m_cbComponent.CurSel();
-	int nComptItem = m_cbComponent.ItemData(nComptSel);
+	int comptSel  = m_componentList.CurSel();
+	int comptItem = m_componentList.ItemData(comptSel);
 
-	ComponentPtr pComponent = g_app.m_components[nComptItem];
+	ComponentPtr component = g_app.m_components[comptItem];
 
 	// Get expected fields.
-	bool bIsClass = pTemplate->m_isClass;
-	bool bHasHPP  = !pTemplate->m_headerFile.empty();
-	bool bHasCPP  = !pTemplate->m_sourceFile.empty();
+	bool isClass = templt->m_isClass;
+	bool hasHpp  = !templt->m_headerFile.empty();
+	bool hasCpp  = !templt->m_sourceFile.empty();
 
 	// Validate class name.
-	CString strClassName = m_ebClassName.Text();
+	CString className = m_classNameEditor.Text();
 
-	if (bIsClass && strClassName.Empty())
+	if (isClass && className.Empty())
 	{
 		AlertMsg(TXT("You must supply a name for the class."));
-		m_ebClassName.Focus();
+		m_classNameEditor.Focus();
 		return;
 	}
 
 	// Validate folder.
-	CPath strFolder = m_cbFolder.Text();
+	CPath folder = m_folderList.Text();
 
-	if (!strFolder.Exists())
+	if (!folder.Exists())
 	{
 		AlertMsg(TXT("The destination folder is invalid."));
-		m_cbFolder.Focus();
+		m_folderList.Focus();
 		return;
 	}
 
 	// Validate HPP file name, if expected.
-	CPath strHPPFile = m_ebHPPFile.Text();
+	CPath hppFile = m_hppFileEditor.Text();
 
-	if (bHasHPP && strHPPFile.Empty())
+	if (hasHpp && hppFile.Empty())
 	{
 		AlertMsg(TXT("You must supply the HPP file name."));
-		m_ebHPPFile.Focus();
+		m_hppFileEditor.Focus();
 		return;
 	}
 
 	// Validate CPP file name, if expected.
-	CPath strCPPFile = m_ebCPPFile.Text();
+	CPath cppFile = m_cppFileEditor.Text();
 
-	if (bHasCPP && strCPPFile.Empty())
+	if (hasCpp && cppFile.Empty())
 	{
 		AlertMsg(TXT("You must supply the CPP file name."));
-		m_ebCPPFile.Focus();
+		m_cppFileEditor.Focus();
 		return;
 	}
 
 	Params params;
 
-	params.add(TXT("Class"), tstring(strClassName));
-	params.add(TXT("Component"), pComponent->m_name);
-	params.add(TXT("Include"), pComponent->m_include);
-	params.add(TXT("Comment"), pComponent->m_comment);
+	params.add(TXT("Class"), tstring(className));
+	params.add(TXT("Component"), component->m_name);
+	params.add(TXT("Include"), component->m_include);
+	params.add(TXT("Comment"), component->m_comment);
 	params.add(TXT("Namespace"), tstring(m_namespaceEditor.Text()));
 	params.add(TXT("Author"), g_app.m_author);
 
 	// Generate HPP file, if required.
-	if (!pTemplate->m_headerFile.empty())
+	if (!templt->m_headerFile.empty())
 	{
-		CPath   strTemplateFile = CPath(g_app.m_strTmplFolder, pTemplate->m_headerFile.c_str());
-		CString strFileTitle    = strHPPFile.FileTitle();
-		CString strFileExt      = strHPPFile.FileExt();
+		CPath   templateFile = CPath(g_app.m_templatesFolder, templt->m_headerFile.c_str());
+		CString fileTitle    = hppFile.FileTitle();
+		CString fileExt      = hppFile.FileExt();
 
-		if (!strFileExt.Empty())
-			strFileExt.Delete(0);
+		// Remove leading '.'.
+		if (!fileExt.Empty())
+			fileExt.Delete(0);
 
-		params.add(TXT("File.Ext"), tstring(strHPPFile));
-		params.add(TXT("File"), tstring(strFileTitle));
-		params.add(TXT("Ext"), tstring(strFileExt));
-		params.add(TXT("HdrExt"), tstring(strFileExt));
+		params.add(TXT("File.Ext"), tstring(hppFile));
+		params.add(TXT("File"), tstring(fileTitle));
+		params.add(TXT("Ext"), tstring(fileExt));
+		params.add(TXT("Header"), tstring(hppFile));
 
-		if (!g_app.m_appCmds.generateFile(strTemplateFile, CPath(strFolder, strHPPFile), params))
+		CPath targetFile(folder, hppFile);
+
+		if (!g_app.m_appCmds.generateFile(templateFile, targetFile, params))
 			return;
 	}
 
 	// Generate CPP file, if required.
-	if (!pTemplate->m_sourceFile.empty())
+	if (!templt->m_sourceFile.empty())
 	{
-		CPath   strTemplateFile = CPath(g_app.m_strTmplFolder, pTemplate->m_sourceFile.c_str());
-		CString strFileTitle    = strCPPFile.FileTitle();
-		CString strFileExt      = strCPPFile.FileExt();
+		CPath   templateFile = CPath(g_app.m_templatesFolder, templt->m_sourceFile.c_str());
+		CString fileTitle    = cppFile.FileTitle();
+		CString fileExt      = cppFile.FileExt();
 
-		if (!strFileExt.Empty())
-			strFileExt.Delete(0);
+		// Remove leading '.'.
+		if (!fileExt.Empty())
+			fileExt.Delete(0);
 
-		params.add(TXT("File.Ext"), tstring(strCPPFile));
-		params.add(TXT("File"), tstring(strFileTitle));
-		params.add(TXT("Ext"), tstring(strFileExt));
+		params.add(TXT("File.Ext"), tstring(cppFile));
+		params.add(TXT("File"), tstring(fileTitle));
+		params.add(TXT("Ext"), tstring(fileExt));
 
-		if (!g_app.m_appCmds.generateFile(strTemplateFile, CPath(strFolder, strCPPFile), params))
+		CPath targetFile(folder, cppFile);
+
+		if (!g_app.m_appCmds.generateFile(templateFile, targetFile, params))
 			return;
 	}
 
 	// Add folder to MRU list.
-	if (g_app.m_astrFolders.Find(strFolder, true) == -1)
-		g_app.m_astrFolders.Add(strFolder);
+	if (g_app.m_mruFolders.Find(folder, true) == -1)
+		g_app.m_mruFolders.Add(folder);
 
-	if (!strClassName.Empty())
-		NotifyMsg(TXT("%s class generated."), strClassName);
+	if (!className.Empty())
+		NotifyMsg(TXT("%s class generated."), className);
 	else
-		NotifyMsg(TXT("%s file generated."), strHPPFile);
+		NotifyMsg(TXT("%s file generated."), hppFile);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//!
+//! Show the HelpFile.
 
-void AppDlg::OnAbout()
+void AppDlg::onHelp()
+{
+	CBusyCursor busyCursor;
+
+	::ShellExecute(NULL, NULL, CPath::ApplicationDir() / TXT("ClassGen.mht"), NULL, NULL, SW_SHOW);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Show the application version.
+
+void AppDlg::onAbout()
 {
 	AboutDlg aboutDlg;
 
